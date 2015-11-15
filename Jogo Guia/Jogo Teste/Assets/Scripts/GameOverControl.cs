6f6a979c3b119daa32648c223105c4f7d3f4fcdf
Tiago Belem <tiago.mb95@gmail.com>
2015-11-15 17:24:58 -0200

    Adicionando projeto guia

---
+++ Jogo Guia/Jogo Teste/Assets/Scripts/GameOverControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverControl : MonoBehaviour {
+
+    private float currTime;
+    
+    // Use this for initialization
+	void Start () {
+        currTime = Time.timeScale;
+        Time.timeScale = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Time.timeScale = currTime;
+            Application.LoadLevel("scene");
+        }
+	
+	}
+}
