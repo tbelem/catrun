eac09255828e85c3382f695262424ee12280c9d5
viniciusmrossi <vinicius07@hotmail.com>
2015-11-30 20:41:32 -0200

    Tela de gameover e demais prontas

---
+++ Catrun/CatrunProject/Assets/Resources/Interface/Scripts/gameoverScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class gameoverScript : MonoBehaviour {
+
+	Text numMoedas;
+	Text pontos;
+
+	void Awake () {
+		numMoedas = GameObject.Find("TextMoedas").GetComponent<Text>();
+		pontos = GameObject.Find("TextPontos").GetComponent<Text>();
+		numMoedas.text = "$" + getMoedas ().ToString ();
+		pontos.text = getPontos ().ToString ();
+	}
+
+	public int getMoedas (){
+		//codigo para buscar do banco
+		return 500;
+	}
+
+	public int getPontos (){
+		//codigo para buscar do banco
+		return 10000;
+	}
+}
